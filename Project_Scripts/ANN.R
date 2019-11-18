11c7b16da0f6917b3d05059cc183b7531d1aef09
Ruben Ernst <57605025+RubenErnst@users.noreply.github.com>
2019-11-18 20:50:03 +0100

    Update ANN.R

--- Project_Scripts/ANN.R
+++ Project_Scripts/ANN.R
@@ -16,8 +16,8 @@ standFun = function(x){
 }
 
 # Load cleaned data
-load("../Data/CleanData/CleanClass2007to2014.RData")
-cleanData <- as_tibble(CleanClass2007to2014)
+load("../Data/CleanData/CleanClass2007to2014_2.RData")
+cleanData <- as_tibble(CleanClass2007to2014_2)
 
 # Define the ANN cost function
 ANN_cost <- function(ANN_par, L_i_size, L_h_size, L_o_size, x, y, lambda){
@@ -47,8 +47,8 @@ ANN_cost <- function(ANN_par, L_i_size, L_h_size, L_o_size, x, y, lambda){
   J = vector()
   
   for (i in 1:n){
-    J[i] = (sum( -y_new[i, ] %*% log(H[i,]) - (1-y_new[i, ]) %*% log(1-H[i,]))) 
-    + (lambda/(2*n)) * (sum(sum( theta1[, 2:dim(theta1)[2] ]^2)) +  sum(sum( theta2[, 2:dim(theta2)[2] ]^2)) ) 
+    J[i] = (sum(-y_new[i,] %*% log(H[i,]) - (1-y_new[i,]) %*% log(1-H[i,]))) 
+    + (lambda/(2*n)) * (sum(sum(theta1[,2:dim(theta1)[2] ]^2)) +  sum(sum(theta2[,2:dim(theta2)[2]]^2)) ) 
   }
   
   J_new = sum(J)/n
@@ -107,9 +107,9 @@ ANN_grad <- function(ANN_par, L_i_size, L_h_size, L_o_size, x, y, lambda){
   }
   
   theta1_grad <- D1/n
-  theta1_grad[,2:ncol(theta1_grad)] <- theta1_grad[,2:ncol(theta1_grad)] + (lambda/n) * theta1[,2:ncol(theta1_grad)]
+  theta1_grad[,2:ncol(theta1_grad)] <- theta1_grad[, 2:ncol(theta1_grad)] + (lambda/n) * theta1[, 2:ncol(theta1_grad)]
   theta2_grad <- D2/n
-  theta2_grad[,2:ncol(theta2_grad)] <- theta2_grad[,2:ncol(theta2_grad)] + (lambda/n) * theta2[,2:ncol(theta2_grad)]
+  theta2_grad[,2:ncol(theta2_grad)] <- theta2_grad[, 2:ncol(theta2_grad)] + (lambda/n) * theta2[, 2:ncol(theta2_grad)]
   
   grad <- c(as.vector(theta1_grad), as.vector(theta2_grad))
 }
@@ -117,10 +117,10 @@ ANN_grad <- function(ANN_par, L_i_size, L_h_size, L_o_size, x, y, lambda){
 # ANN for QB ----
 
 # Select QBs and remove any players with missing values
-cleanData_QB <- cleanData %>% filter(., Position.x == "QB") %>% select(., -X40YD, -Vertical, -Broad.Jump, -BenchReps, -Shuttle) %>% drop_na(.)
+cleanData_QB <- cleanData %>% filter(., Position == "QB") %>% drop_na(.)
 
 # Select years 2007 through 2013 as training data
-x <- as.matrix(cleanData_QB %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name.x, -Team.Code.x, -Class.x, -Position.x, -Year, -Drafted))
+x <- as.matrix(cleanData_QB %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
 y <- pull(cleanData_QB %>% filter(., Year < 2014) %>% select(., Drafted))
 
 # Standardize the training data
@@ -163,8 +163,8 @@ theta1_train <- matrix(data = BP_par[1:(L_h_size * (L_i_size + 1))], nrow = L_h_
 theta2_train <- matrix(data = BP_par[(1 + (L_h_size * (L_i_size + 1))):length(BP_par)], nrow = L_o_size)
 
 # Take the year 2014 as testing data
-cleanData_QB_test <- cleanData %>% filter(., Position.x == "QB", Year == 2014) %>% select(., -X40YD, -Vertical, -Broad.Jump, -BenchReps, -Shuttle) %>% drop_na(.)
-x_test <- as.matrix(cleanData_QB_test %>% select(., -Player.Code, -Name.x, -Team.Code.x, -Class.x, -Position.x, -Year, -Drafted))
+cleanData_QB_test <- cleanData %>% filter(., Position == "QB", Year == 2014) %>% drop_na(.)
+x_test <- as.matrix(cleanData_QB_test %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
 y_test <- pull(cleanData_QB_test %>% select(., Drafted))
 
 # Standardize the testing data and replace NA with zero
@@ -179,15 +179,15 @@ a_2 <- rbind(1, sigmoid(theta1_train %*% a_1))
 a_3 <- sigmoid(theta2_train %*% a_2)
 
 # Summarize the results
-pred_QB <- tibble("Code" = cleanData_QB_test$Player.Code, "Name" = cleanData_QB_test$Name.x, "pred" = a_3, "Drafted" = cleanData_QB_test$Drafted)
+pred_QB <- tibble("Code" = cleanData_QB_test$Player.Code, "Name" = cleanData_QB_test$Name, "pred" = as.vector(a_3), "Drafted" = cleanData_QB_test$Drafted)
 
 # ANN for RB ----
 
 # Select RBs and remove any players with missing values
-cleanData_RB <- cleanData %>% filter(., Position.x == "RB") %>% select(., -X40YD, -Vertical, -Broad.Jump, -BenchReps, -Shuttle) %>% drop_na(.)
+cleanData_RB <- cleanData %>% filter(., Position == "RB") %>% drop_na(.)
 
 # Select years 2007 through 2013 as training data
-x <- as.matrix(cleanData_RB %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name.x, -Team.Code.x, -Class.x, -Position.x, -Year, -Drafted))
+x <- as.matrix(cleanData_RB %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
 y <- pull(cleanData_RB %>% filter(., Year < 2014) %>% select(., Drafted))
 
 # Standardize the training data
@@ -230,8 +230,8 @@ theta1_train <- matrix(data = BP_par[1:(L_h_size * (L_i_size + 1))], nrow = L_h_
 theta2_train <- matrix(data = BP_par[(1 + (L_h_size * (L_i_size + 1))):length(BP_par)], nrow = L_o_size)
 
 # Take the year 2014 as testing data
-cleanData_RB_test <- cleanData %>% filter(., Position.x == "RB", Year == 2014) %>% select(., -X40YD, -Vertical, -Broad.Jump, -BenchReps, -Shuttle) %>% drop_na(.)
-x_test <- as.matrix(cleanData_RB_test %>% select(., -Player.Code, -Name.x, -Team.Code.x, -Class.x, -Position.x, -Year, -Drafted))
+cleanData_RB_test <- cleanData %>% filter(., Position == "RB", Year == 2014) %>%  drop_na(.)
+x_test <- as.matrix(cleanData_RB_test %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
 y_test <- pull(cleanData_RB_test %>% select(., Drafted))
 
 # Standardize the testing data and replace NA with zero
@@ -246,15 +246,15 @@ a_2 <- rbind(1, sigmoid(theta1_train %*% a_1))
 a_3 <- sigmoid(theta2_train %*% a_2)
 
 # Summarize the results
-pred_RB <- tibble("Code" = cleanData_RB_test$Player.Code, "Name" = cleanData_RB_test$Name.x, "pred" = a_3, "Drafted" = cleanData_RB_test$Drafted)
+pred_RB <- tibble("Code" = cleanData_RB_test$Player.Code, "Name" = cleanData_RB_test$Name, "pred" = as.vector(a_3), "Drafted" = cleanData_RB_test$Drafted)
 
 # ANN for WR ----
 
 # Select WRs and remove any players with missing values
-cleanData_WR <- cleanData %>% filter(., Position.x == "WR") %>% select(., -X40YD, -Vertical, -Broad.Jump, -BenchReps, -Shuttle) %>% drop_na(.)
+cleanData_WR <- cleanData %>% filter(., Position == "WR") %>% drop_na(.)
 
 # Select years 2007 through 2013 as training data
-x <- as.matrix(cleanData_WR %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name.x, -Team.Code.x, -Class.x, -Position.x, -Year, -Drafted))
+x <- as.matrix(cleanData_WR %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
 y <- pull(cleanData_WR %>% filter(., Year < 2014) %>% select(., Drafted))
 
 # Standardize the training data
@@ -297,8 +297,8 @@ theta1_train <- matrix(data = BP_par[1:(L_h_size * (L_i_size + 1))], nrow = L_h_
 theta2_train <- matrix(data = BP_par[(1 + (L_h_size * (L_i_size + 1))):length(BP_par)], nrow = L_o_size)
 
 # Take the year 2014 as testing data
-cleanData_WR_test <- cleanData %>% filter(., Position.x == "WR", Year == 2014) %>% select(., -X40YD, -Vertical, -Broad.Jump, -BenchReps, -Shuttle) %>% drop_na(.)
-x_test <- as.matrix(cleanData_WR_test %>% select(., -Player.Code, -Name.x, -Team.Code.x, -Class.x, -Position.x, -Year, -Drafted))
+cleanData_WR_test <- cleanData %>% filter(., Position == "WR", Year == 2014) %>% drop_na(.)
+x_test <- as.matrix(cleanData_WR_test %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
 y_test <- pull(cleanData_WR_test %>% select(., Drafted))
 
 # Standardize the testing data and replace NA with zero
@@ -313,4 +313,95 @@ a_2 <- rbind(1, sigmoid(theta1_train %*% a_1))
 a_3 <- sigmoid(theta2_train %*% a_2)
 
 # Summarize the results
-pred_WR <- tibble("Code" = cleanData_WR_test$Player.Code, "Name" = cleanData_WR_test$Name.x, "pred" = a_3, "Drafted" = cleanData_WR_test$Drafted)
+pred_WR <- tibble("Code" = cleanData_WR_test$Player.Code, "Name" = cleanData_WR_test$Name, "pred" = as.vector(a_3), "Drafted" = cleanData_WR_test$Drafted)
+
+# ANN for all positions ----
+
+# Remove any players with missing values
+cleanData_all <- cleanData %>% drop_na(.)
+
+# Select years 2007 through 2013 as training data
+x <- as.matrix(cleanData_all %>% filter(., Year < 2014) %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
+y <- pull(cleanData_all %>% filter(., Year < 2014) %>% select(., Drafted))
+
+# Standardize the training data
+for (i in 1:ncol(x)) {
+  x[,i] <- standFun(x[,i])
+}
+
+# Replace variables that are consistently zero and therefore yield NA when standardized with zero again
+x[is.na(x)] <- 0
+
+# Parameters of ANN
+L_i_size <- ncol(x)
+L_h_size <- 10
+L_o_size <-  1
+n <- length(y)
+theta1 <- matrix(1, nrow = L_h_size, ncol = L_i_size + 1)
+theta2 <- matrix(1, nrow = L_o_size, ncol = L_h_size + 1)
+ANN_par <- c(as.vector(theta1), as.vector(theta2))
+
+# Make verbose to print every 5th iteration
+options <- list(trace = 5)
+
+# Optimize cost function
+BP_pred <- nlminb(start = ANN_par,
+                  objective = ANN_cost, 
+                  gradient = ANN_grad,
+                  hessian = NULL,
+                  L_i_size = L_i_size,
+                  L_h_size = L_h_size,
+                  L_o_size = L_o_size,
+                  x = x, y = y,
+                  lambda = 1,
+                  control = options)
+
+# Retrieve the theta vector found through backpropagation
+BP_par <- BP_pred$par
+
+# Separate theta vector from backpropagation into thetas
+theta1_train <- matrix(data = BP_par[1:(L_h_size * (L_i_size + 1))], nrow = L_h_size)
+theta2_train <- matrix(data = BP_par[(1 + (L_h_size * (L_i_size + 1))):length(BP_par)], nrow = L_o_size)
+
+# Take the year 2014 as testing data
+cleanData_all_test <- cleanData %>% filter(., Year == 2014) %>% drop_na(.)
+x_test <- as.matrix(cleanData_all_test %>% select(., -Player.Code, -Name, -Class, -Position, -Year, -Drafted))
+y_test <- pull(cleanData_all_test %>% select(., Drafted))
+
+# Standardize the testing data and replace NA with zero
+for (i in 1:ncol(x_test)) {
+  x_test[,i] <- standFun(x_test[,i])
+}
+x_test[is.na(x_test)] <- 0
+
+# Make predictions with the thetas found in backpropagation
+a_1 <- rbind(1, t(x_test))
+a_2 <- rbind(1, sigmoid(theta1_train %*% a_1))
+a_3 <- sigmoid(theta2_train %*% a_2)
+
+# Summarize the results
+pred_all <- tibble("Code" = cleanData_all_test$Player.Code, "Name" = cleanData_all_test$Name, "pred" = as.vector(a_3), "Drafted" = cleanData_all_test$Drafted)
+
+# Aggregate Results ----
+resultsComb_separate <- tibble("Player.Code" = c(pred_QB$Code, pred_RB$Code, pred_WR$Code),
+                          "Name" = c(pred_QB$Name, pred_RB$Name, pred_WR$Name),
+                          "P" = c(pred_QB$pred, pred_RB$pred, pred_WR$pred),
+                          "Pred" = ifelse(c(pred_QB$pred, pred_RB$pred, pred_WR$pred) >= 0.5, 1, 0),
+                          "Drafted" = c(pred_QB$Drafted, pred_RB$Drafted, pred_WR$Drafted))
+resultsComb_separate <- resultsComb_separate %>% mutate(., "error" = ifelse(Pred != Drafted, 1, 0),
+                                              "TP" = ifelse(Pred == Drafted & Drafted == 1, 1, 0),
+                                              "FP" = ifelse(Pred != Drafted & Drafted == 0, 1, 0),
+                                              "TN" = ifelse(Pred == Drafted & Drafted == 0, 1, 0),
+                                              "FN" = ifelse(Pred != Drafted & Drafted == 1, 1, 0))
+
+resultsComb_all <- tibble("Player.Code" = pred_all$Code,
+                          "Name" = pred_all$Name,
+                          "P" = pred_all$pred,
+                          "Pred" = ifelse(pred_all$pred >= 0.5, 1, 0),
+                          "Drafted" = pred_all$Drafted)
+resultsComb_all <- resultsComb_all %>% mutate(., "error" = ifelse(Pred != Drafted, 1, 0),
+                                              "TP" = ifelse(Pred == Drafted & Drafted == 1, 1, 0),
+                                              "FP" = ifelse(Pred != Drafted & Drafted == 0, 1, 0),
+                                              "TN" = ifelse(Pred == Drafted & Drafted == 0, 1, 0),
+                                              "FN" = ifelse(Pred != Drafted & Drafted == 1, 1, 0))
+
